ff5a1765cd225e5a2f82b134265dc74ee08eb5be
longislandicetea <haoluo3@illinois.edu>
2015-04-19 20:30:57 -0500

    define custom json encodeer and decoder for message class

--- message.py
+++ message.py
@@ -1,14 +1,27 @@
-import datetime
 import json
 from json import JSONEncoder
 from json import JSONDecoder
-import yaml
 
 from enum_type import MSG_TYPE
 
 class MessageEncoder(JSONEncoder):
-	def default(self, obj):
-		return obj.__json__()
+	def encode(self, obj):
+		obj_dict = dict()
+		obj_dict['msg_type'] = int(obj.msg_type)
+		obj_dict['src'] = obj.src
+		obj_dict['dest'] = obj.dest
+		obj_dict['data'] = obj.data
+		return super(MessageEncoder, self).encode(obj_dict)
+
+class MessageDecoder(JSONDecoder):
+	def decode(self, json_string):
+		parsed_dict = super(MessageDecoder, self).decode(json_string)
+		return Message(
+			MSG_TYPE(parsed_dict['msg_type']),
+			parsed_dict['src'],
+			parsed_dict['dest'],
+			parsed_dict['data'],
+			)
 
 class Message(object):
 	def __init__(self, msg_type, src, dest, data):
@@ -18,7 +31,12 @@ class Message(object):
 		self.data = data
 
 	def __json__(self):
-		return dict(msg_type=self.msg_type, src=self.src, dest=self.dest, data=self.data)
+		return dict(
+			msg_type=self.msg_type, 
+			src=self.src, 
+			dest=self.dest, 
+			data=self.data,
+			)
 
 	def ToJSON(self):
 		return json.dumps(self, cls=MessageEncoder)
